590eb65bf753847e6b60295944e53be6d1294279
Usman Lari <usmanlari2000@gmail.com>
2024-08-08 20:29:56 +0500

    changed action button text

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
           className="block select-none h-10 w-fit bg-black hover:opacity-70 transition-all duration-200 mx-auto my-5.5 px-5.5 rounded-full flex gap-x-1.5 items-center text-white text-0.5sm"
           href="https://wa.me/923211681128?text=Hi,%20I'm%20interested%20in%20having%20a%20free%20website%20made."
         >
-          <div>Send message</div>
+          <div>WhatsApp Us</div>
           <svg
             width="0.625rem"
             viewBox="0 0 10 10"
