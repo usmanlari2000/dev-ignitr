e84df6fc9beb30989de81bf1e71ceb2c5a933891
Usman Lari <usmanlari2000@gmail.com>
2024-08-08 23:20:29 +0500

    added google tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,25 @@
 import "./globals.css";
+import Head from "next/head";
+import Script from "next/script";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { Head } from "next/document";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <script
+        <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-11184509845"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-11184509845');
-            `,
-          }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              gtag('event', 'conversion', {'send_to': 'AW-11184509845/F3TBCMPLmskZEJWnmNUp'});
-            `,
-          }}
-        />
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11184509845');`}
+        </Script>
+        <Script>{`gtag('event', 'conversion', {'send_to': 'AW-11184509845/F3TBCMPLmskZEJWnmNUp'});`}</Script>
       </Head>
       <body>
         <Header />
